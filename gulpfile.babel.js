a7881ba3d3ac61647f41546b8e522515b9524f2d
Chris Keller <kellerch@umich.edu>
2018-08-13 15:00:46 -0400

    Updated lots of things

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,19 +3,47 @@
 import gulp from "gulp";
 import cssnano from "cssnano";
 import babel from "gulp-babel";
+import uncss from "postcss-uncss";
 import postcss from "gulp-postcss";
+import imagemin from "gulp-imagemin";
 import BrowserSync from "browser-sync";
 import cssImport from "postcss-import";
+import imageminSvgo from "imagemin-svgo";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminPngquant from "imagemin-pngquant";
 import postcssPresetEnv from "postcss-preset-env";
 
 const browserSync = BrowserSync.create();
 
+// Images task.
+gulp.task("images", () => {
+  // Handle JPEGs
+  gulp.src("./src/img/**/*.jpg")
+    .pipe(imagemin([imageminMozjpeg({ quality: 80 })], {
+      progressive: true,
+    }))
+    .pipe(gulp.dest("./web/img"));
+
+  // Handle PNGs.
+  gulp.src("./src/img/**/*.png")
+    .pipe(imagemin([imageminPngquant({ strip: true })]))
+    .pipe(gulp.dest("./web/img"));
+
+  // Handle SVGs
+  gulp.src("./src/img/**/*.svg")
+    .pipe(imagemin([imageminSvgo()]))
+    .pipe(gulp.dest("./web/img"));
+});
+
 // CSS task.
 gulp.task("css", () => {
-  gulp.src("./src/css/*.css")
+  gulp.src("./src/css/main.css")
     .pipe(postcss([
       cssImport({ from: "./src/css/main.css" }),
       postcssPresetEnv(),
+      uncss({
+        html: ["./web/**/*.html"]
+      }),
       cssnano(),
     ]))
     .pipe(gulp.dest("./web"))
@@ -34,19 +62,25 @@ gulp.task("js", (done) => {
 
 // HTML task.
 gulp.task("html", (done) => {
+  gulp.src("./src/html/*.html")
+    .pipe(gulp.dest("./web"));
+
   browserSync.reload();
   done();
 });
 
 // Default task.
-gulp.task("default", ["css", "js", "html"], () => {  
+gulp.task("default", ["html", "css", "js", "images"], () => {  
   browserSync.init({
+    browser: [],
     server: {
       baseDir: "./web"
     }
   });
 
-  gulp.watch("./src/css/*.css", ["css"]);
-  gulp.watch("./src/js/*.js", ["js"]);
-  gulp.watch("./web/*.html", ["html"]);
+  gulp.watch("./src/css/**/*.css", ["css"]);
+  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/html/**/*.html", ["html", "css"]);
+
+  gulp.watch("src/img/**/*", ["images"]);
 });
